53ad8c9f647ba0ed3265e0e3eb4c2d6d016085e9
Hovo-93 <ovo.aroyan@mail.ru>
2022-12-02 17:23:28 +0300

    create model dynamic

--- app/models/User.php
+++ app/models/User.php
@@ -1,10 +1,13 @@
 <?php
 
 namespace app\models;
-
 use src\database\Model;
 
 class User extends Model
 {
-
+    public function getUserData(){
+        return parent::select('email','password')->from($this->getClassName())->first();
+    }
 }
+
+
